b7dbc00f05456d4613fc64c0b345a5e65966cb4d
Fabian Sellemond <fabian.sellemond@wienit.at>
2020-07-17 15:36:52 +0200

    viewchild => viewchildren

--- src/app/configuration-view-switcher/configuration-view-switcher.component.ts
+++ src/app/configuration-view-switcher/configuration-view-switcher.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChildren,
+  QueryList,
+} from '@angular/core';
 import { ConfigurationStepType } from '../model/configurationstep-type';
-import { BestattungsartComponent } from '../configuration-view/bestattungsart/bestattungsart.component';
-import { GrabstelleComponent } from '../configuration-view/grabstelle/grabstelle.component';
-import { VerabschiedungsfeierComponent } from '../configuration-view/verabschiedungsfeier/verabschiedungsfeier.component';
-import { PaketauswahlComponent } from '../configuration-view/paketauswahl/paketauswahl.component';
-import { BeguenstigterComponent } from '../configuration-view/beguenstigter/beguenstigter.component';
 import { Store } from '@ngxs/store';
 import { ConfigruationChangedContract } from '../configuration-view/configuration-changed-contract';
 import { ConfigurationChangeAction } from '../store/vorsorge-configuration.actions';
-import { AbstractConfiguration } from '../configuration-view/abstract-configuration';
 
 @Component({
   selector: 'app-configuration-view-switcher',
@@ -20,38 +20,18 @@ export class ConfigurationViewSwitcherComponent implements OnInit {
 
   @Input() configurationType: ConfigurationStepType;
 
-  @ViewChild(BestattungsartComponent)
-  bestattungsArtComponent: BestattungsartComponent;
-
-  @ViewChild(GrabstelleComponent)
-  grabstelleComponent: GrabstelleComponent;
-
-  @ViewChild(VerabschiedungsfeierComponent)
-  verabschiedungsfeierComponent: VerabschiedungsfeierComponent;
-
-  @ViewChild(PaketauswahlComponent)
-  paketauswahlComponent: PaketauswahlComponent;
-
-  @ViewChild(BeguenstigterComponent)
-  beguenstigterComponent: BeguenstigterComponent;
+  @ViewChildren('abstractConfiguration')
+  abstractConfigurations: QueryList<ConfigruationChangedContract<any>>; // typesafety unknown extends XY
 
   ngOnInit(): void {}
 
   public handleConfigurationUpdates() {
-    const component: ConfigruationChangedContract<any> = this.getActiveViewChild();
+    const component: ConfigruationChangedContract<any> = this
+      .abstractConfigurations.first;
+
     if (component.isConfigurationChanged()) {
       const action: ConfigurationChangeAction = component.getAction();
       this.store.dispatch(action);
     }
-  }
-
-  getActiveViewChild(): AbstractConfiguration<ConfigurationChangeAction> {
-    return [
-      this.bestattungsArtComponent,
-      this.grabstelleComponent,
-      this.verabschiedungsfeierComponent,
-      this.paketauswahlComponent,
-      this.beguenstigterComponent,
-    ].filter((c) => c !== undefined)[0];
   }
 }
